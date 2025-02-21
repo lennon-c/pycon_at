9b01234e7389e0613b235835b2bafc64512d3524
Carolina Lennon <14179052+lennon-c@users.noreply.github.com>
2025-02-21 15:22:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## A Hands-On Workshop on Parsing Wikitext
+[PyCon Austria](https://pycon.pyug.at/en/)
+6 and 7 April 2025 | Eisenstadt
+
+
+
+Here you can find the notebooks, python files and data use in the workshop. 
+
---
+++ python/0-Special_Exports.py
@@ -0,0 +1,77 @@
+
+
+"""The **Special Export** tool fetches specific pages with their raw content (*wikitext*) in real-time, without needing to download the entire dataset. The content is provided in XML format.
+
+
+
+## Importing Packages
+"""
+
+import requests # to fetch info from URLs
+
+"""
+
+## Using the **Special Export** Tool
+
+You can actually use **Special:Export** to retrieve pages from *any* Wiki site. On the German Wiktionary, however, the tool is labelled **Spezial:Exportieren**, but it works the same way.
+
+
+**Exporting Pages from Any Wiki Site**
+
+To access the XML content of the page titled "Austria" from English Wikipedia, you can use the following Python code. When you press `run`, it will open the export link in your default browser:
+"""
+
+title = 'Austria'
+domain = 'en.wikipedia.org'
+url = f'https://{domain}/wiki/Special:Export/{title}'
+print(url)
+
+"""
+
+**Exporting Pages from the German Wiktionary**
+
+For the German Wiktionary, the export tool uses `Spezial:Exportieren` instead of `Special:Export`. You can use similar Python code to open the export link for the page titled "hoch" (German for "high"):
+"""
+
+title = 'hoch'
+domain = 'de.wiktionary.org'
+url = f'https://{domain}/wiki/Spezial:Exportieren/{title}'
+print(url)
+
+"""
+
+## Fetching XML Data with `requests`
+
+
+To programmatically fetch and download XML content, you can use Python's `requests` library. This example shows how to build the URL, make a request, and get the XML content of a Wiktionary page by its title.
+"""
+
+def fetch(title):
+    # Construct the URL for the XML export of the given page title
+    url = f'https://de.wiktionary.org/wiki/Spezial:Exportieren/{title}'
+    
+    # Send a GET request
+    resp = requests.get(url)
+    
+    # Check if the request was successful, and raise an error if not
+    resp.raise_for_status()
+    
+    # Return the XML content of the requested page
+    return resp.text
+
+"""
+
+Next, let us attempt to retrieve the XML content for the page titled "hoch" and print the initial 500 bytes for a glimpse of the XML content.
+"""
+
+page = fetch('hoch')
+print(page[:500])
+
+"""
+
+We will continue to use the `fetch` function throughout this tutorial.
+
+
+
+"""
+
